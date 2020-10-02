d2227366265466907b7f9b1081d2afa0856518e4
Tatiana Ampilogova <ampi.tatyana@gmail.com>
2020-10-02 10:48:22 -0700

    minor fixes

--- Join/Join/ProfileViewController.swift
+++ Join/Join/ProfileViewController.swift
@@ -20,7 +20,7 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     override func viewDidLoad() {
         super.viewDidLoad()
         userNameLabel.text = "Tatiana Ampilogova"
-        userInfoLabel.text = "About me"
+        userInfoLabel.text = "I am an enthusiastic, responsible, and hard-working iOS Developer (Swift) with 1+ years of experience in design, development, testing, and implementation of various iOS projects."
         avatarView.configure(userNameLabel.text ?? "")
         avatarView.avatarButton.addTarget(self, action: #selector(setupAvatar), for: .touchUpInside)
     }
